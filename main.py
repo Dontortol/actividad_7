894043559bbd4cf971df213e5d8e5c6069906741
rodri <rodrihercar24@gmail.com>
2025-07-24 20:00:26 -0600

    termine mas definiciones para luego hacer el menu

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def datos():
                         case "2":
                             print("Regresando al menu")
                             break
+                        case _:
+                            print("Opcion invalida")
             case "2":
                 print("Datos regresados")
                 positivo = 0
@@ -41,4 +43,59 @@ def datos():
                       f"La cantidad total de positivos: {positivo}\n"
                       f"La cantidad total de negativos: {negativo}\n"
                       f"La cantidad total de ceros: {cero}\n")
-datos()
+                break
+            case "3":
+                print("Regresando al menu")
+                break
+            case _:
+                print("opcion no valida")
+
+def area_triangulo(a, b):
+    area = a * b / 2
+    print(f"El area del triangulo es de: {area}")
+
+def perimetro_triangulo(a, b, c):
+    perimeter = a + b + c
+    print(f"El perimetro del triangulo es de: {perimeter}")
+
+def n_primo():
+    a = int(input("Ingresar un numero: "))
+    if a % 2 == 0:
+        print("El numero no es primo")
+    else:
+        print("El numero es primo")
+
+def promedio():
+    promedio = []
+    while True:
+        print("1. Ingresar calificacion"
+              "2. ver resultados"
+              "3. regresar al menu\n")
+        select_calificacion = input("Seleccione una opcion: ")
+        match select_calificacion:
+            case "1":
+                while True:
+                    print("1. Ingresar calificacion\n"
+                          "2. regresar al menu\n")
+                    select1 = input("Seleccione una opcion: ")
+                    match select1:
+                        case "1":
+                            calificacion = int(input("Ingresar calificacion: "))
+                            promedio.append(calificacion)
+                        case "2":
+                            print("Regresando al menu")
+                            break
+                        case _:
+                            print("Opcion invalida")
+            case "2":
+                print("Promedio de calificacion")
+                alto = 0
+                riesgo = 0
+                for i in promedio:
+                    if i >= 85:
+                        alto += 1
+                    if i < 60:
+                        riesgo += 1
+                print(f"El promedio de calificacion es de: {sum(promedio)/len(promedio)}\n"
+                      f"Notas que estan arriba de 85: {alto}\n"
+                      f"Notas que estan en zona ade riesgo: {riesgo}\n")
