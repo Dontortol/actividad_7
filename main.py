59a0fd17899507f2ff516ca4603009be12eec16e
rodri <rodrihercar24@gmail.com>
2025-07-24 20:15:00 -0600

    termine las funciones

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from unittest import case
+
+
 def datos():
     suma = []
     while True:
@@ -42,7 +45,8 @@ def datos():
                       f"El promedio total es de: {sum(suma) / len(suma)}\n"
                       f"La cantidad total de positivos: {positivo}\n"
                       f"La cantidad total de negativos: {negativo}\n"
-                      f"La cantidad total de ceros: {cero}\n")
+                      f"La cantidad total de ceros: {cero}\n"
+                      f"la cantidad de multiplos de 3 son: {multiplo}\n")
                 break
             case "3":
                 print("Regresando al menu")
@@ -68,8 +72,8 @@ def n_primo():
 def promedio():
     promedio = []
     while True:
-        print("1. Ingresar calificacion"
-              "2. ver resultados"
+        print("1. Ingresar calificacion\n"
+              "2. ver resultados\n"
               "3. regresar al menu\n")
         select_calificacion = input("Seleccione una opcion: ")
         match select_calificacion:
@@ -99,3 +103,41 @@ def promedio():
                 print(f"El promedio de calificacion es de: {sum(promedio)/len(promedio)}\n"
                       f"Notas que estan arriba de 85: {alto}\n"
                       f"Notas que estan en zona ade riesgo: {riesgo}\n")
+            case "3":
+                print("Regresando al menu")
+                break
+            case _:
+                print("opcion no valida")
+
+def calcular():
+    while True:
+        print("----Bienvenido a la calculadora de operaciones basicas----\n"
+              "1. Suma\n"
+              "2. Resta\n"
+              "3. Multiplicacion\n"
+              "4. Division\n"
+              "5. Salir")
+        calc_select = input("Seleccione una opcion: ")
+        match calc_select:
+            case "1":
+                print("Ingrese dos numeros para ser sumados\n")
+                n1 = int(input("Ingresar primer numero: "))
+                n2 = int(input("Ingresar segundo numero: "))
+                print(f"La suma de {n1} + {n2} es = {n1 + n2}\n")
+            case "2":
+                print("Ingrese dos numeros para ser restados\n")
+                r1 = int(input("Ingresar primer numero: "))
+                r2 = int(input("Ingresar segundo numero: "))
+                print(f"La resta de {r1} + {r2} es = {r1 - r2}")
+            case "3":
+                print("Ingrese dos numeros para ser multiplicacion\n")
+                m1 = int(input("Ingresar primer numero: "))
+                m2 = int(input("Ingresar segundo numero: "))
+                print(f"La multiplicacion de {m1} * {m2} es = {m1 * m2}")
+            case "4":
+                print("Ingrese dos numeros para ser division\n")
+                d1 = int(input("Ingresar primer numero: "))
+                d2 = int(input("Ingresar segundo numero: "))
+                print(f"La division de {d1} / {d2} es = {d1 / d2}")
+
+while True:
